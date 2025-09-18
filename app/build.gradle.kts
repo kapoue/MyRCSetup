35b4aeb607722dd7c4a99271127c2b41afd50654
kapoue <kapoue@gmail.com>
2025-09-18 14:46:44 +0200

    Update version to 1.4 and add changelogs for version 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.myrcsetup.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
